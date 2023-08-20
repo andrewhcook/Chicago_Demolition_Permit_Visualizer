8dc87c07eb2055a069c4629c65c0c0ea06f52f39
Andrew Cook <110200888+andrewhcook@users.noreply.github.com>
2023-08-20 13:48:55 +0000

    feat: solid start, app is functional

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem "puma", "~> 5.0"
 
 # use active record
 gem "sinatra-activerecord"
-
+gem 'google_maps_service', '~> 0.4.2'
+gem 'gmaps4rails'
 group :development do
   gem "better_errors"
   gem "binding_of_caller"
--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+@endpoint_url = "https://data.cityofchicago.org/resource/e4xk-pud8.json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
+end
+
+get("/map_view") do
+erb(:map_view)
+end
+
+get("/records_view/:records_to/:records_from") do 
+erb(:records_view)
+end
+
+get("/filtered_map_view") do
+erb(:filtered_map_view)
 end
