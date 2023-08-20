fcc1150da8a23f5e255781f5420d09d1f74f809e
Andrew Cook <110200888+andrewhcook@users.noreply.github.com>
2023-08-20 19:50:02 +0000

    fix: filtered view now works

--- app.rb
+++ app.rb
@@ -14,6 +14,10 @@ get("/records_view/:records_to/:records_from") do
 erb(:records_view)
 end
 
-get("/filtered_map_view") do
+get("/filtered_map_view/:records_from/:records_to") do
 erb(:filtered_map_view)
+end
+
+get("/filtered_records_view/:anchor_address/:distance/:records_from/:records_to")do
+erb(:filtered_records_view)
 end
