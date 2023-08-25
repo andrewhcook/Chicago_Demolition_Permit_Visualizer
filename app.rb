28c4ca252843a4dc68be5949cc7bbf14bd83f567
Andrew Cook <110200888+andrewhcook@users.noreply.github.com>
2023-08-25 14:42:45 +0000

    removed certain routes to limit google API calls

--- app.rb
+++ app.rb
@@ -1,25 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
-@endpoint_url = "https://data.cityofchicago.org/resource/e4xk-pud8.json"
 
 get("/") do
  erb(:homepage)
 end
 
-get("/map_view/:address") do
-erb(:map_view)
-end
-
-get("/records_view/:records_to/:records_from") do 
-erb(:records_view)
-end
-
 get("/filtered_map_view/:records_from/:records_to") do
 erb(:filtered_map_view)
-end
-
-get("/filtered_records_view/:anchor_address/:distance/:records_from/:records_to")do
-erb(:filtered_records_view)
 end
 
 get("/date_filtered_view") do
